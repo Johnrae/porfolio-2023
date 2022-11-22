35746f37a21962356b7810c190bfdbece0b71193
John Rae <john.rae23@gmail.com>
2022-11-21 22:44:03 -0500

    Install tuna js and build effects into the chain

--- src/hooks/useAudio.ts
+++ src/hooks/useAudio.ts
@@ -4,6 +4,7 @@
 // Signal processing is wild
 
 import { throttle } from 'underscore'
+import Tuna from 'tunajs'
 
 const noteToFrequency = (note: number) => {
   return 440 * Math.pow(2, (note - 69) / 12)
@@ -13,30 +14,61 @@ const setupAudio = () => {
   if (!window) return
 
   const context = new AudioContext()
+  const tuna = new Tuna(context)
 
   // create Oscillator and gain node
   const oscillator = context.createOscillator()
   oscillator.frequency.value = noteToFrequency(48)
   // @ts-ignore
   window.currentNote = 48
-  const gainNode = context.createGain()
-  const echoNode = context.createDelay(100)
-  echoNode.delayTime.value = 0.05
-  gainNode.gain.value = 0.1
+  const input = context.createGain()
+
+  const chorus = new tuna.Chorus({
+    rate: 1.5,
+    feedback: 0.2,
+    delay: 0.0045,
+    bypass: 0,
+  })
+
+  const delay = new tuna.Delay({
+    feedback: 0.45, //0 to 1+
+    delayTime: 100, //1 to 10000 milliseconds
+    wetLevel: 0.5, //0 to 1+
+    dryLevel: 1, //0 to 1+
+    cutoff: 20000, //cutoff frequency of the built in lowpass-filter. 20 to 22050
+    bypass: 0.5,
+  })
+
+  const bitcrusher = new tuna.Bitcrusher({
+    bits: 4, //1 to 16
+    normfreq: 0.1, //0 to 1
+    bufferSize: 4096, //256 to 16384
+  })
+
+  const output = context.createGain()
+  input.gain.value = 0.1
 
   oscillator.type = 'square'
   oscillator.start()
-  echoNode.connect(gainNode).connect(context.destination)
+
+  input.connect(chorus)
+  chorus.connect(output)
+
+  input.connect(bitcrusher)
+  bitcrusher.connect(output)
+
+  input.connect(delay)
+  delay.connect(output)
+
+  output.connect(context.destination)
 
   window.addEventListener('mousedown', () => {
     console.log(oscillator, 'mouse down')
-    oscillator.connect(gainNode)
-    oscillator.connect(echoNode)
+    oscillator.connect(input)
   })
 
   window.addEventListener('mouseup', () => {
-    oscillator.disconnect(gainNode)
-    oscillator.disconnect(echoNode)
+    oscillator.disconnect(input)
   })
 
   const handleMouseMove = throttle((e: MouseEvent) => {
@@ -45,8 +77,7 @@ const setupAudio = () => {
     const x = clientX / innerWidth
     const y = clientY / innerHeight
     console.log('throttling')
-    echoNode.delayTime.value = y * 1.2
-    gainNode.gain.value = x * 0.5
+    input.gain.value = x * 0.5
   }, 100)
 
   window.addEventListener('mousemove', (e: any) => {
@@ -55,6 +86,7 @@ const setupAudio = () => {
   })
 
   window.addEventListener('keydown', (e: any) => {
+    // Modulator keys
     if (e.key === 'f') {
       // @ts-ignore
       window.currentNote += 1
@@ -77,6 +109,7 @@ const setupAudio = () => {
       oscillator.frequency.value += 2
     }
 
+    // Keyboard
     if (e.key === 'z') {
       // @ts-ignore
       window.currentNote = 48
